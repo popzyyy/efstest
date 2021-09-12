2e55b8bc58a8817520b312ebc9acd46a5ea65132
popzyyy <70179524+popzyyy@users.noreply.github.com>
2021-09-12 16:53:20 -0500

    requirements, gitignore, settings

--- efstest/settings.py
+++ efstest/settings.py
@@ -132,6 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
